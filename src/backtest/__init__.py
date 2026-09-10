fb22f66c7de299f0b84a04cb95357d696ca223d6
limhub <zhanggu9@gmail.com>
2026-09-10 16:08:17 +0900

    backtest: export scalping proxy strategy

--- src/backtest/__init__.py
+++ src/backtest/__init__.py
@@ -1,3 +1,10 @@
 from .engine import BacktestConfig, BacktestResult, Trade, run_backtest
+from .strategies import one_minute_scalping_proxy_strategy
 
-__all__ = ["BacktestConfig", "BacktestResult", "Trade", "run_backtest"]
+__all__ = [
+    "BacktestConfig",
+    "BacktestResult",
+    "Trade",
+    "run_backtest",
+    "one_minute_scalping_proxy_strategy",
+]
